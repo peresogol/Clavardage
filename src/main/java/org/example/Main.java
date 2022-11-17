927f99c84a8f4b753a16458f195153a242f5f323
VassiliKan <sachacauli@gmail.com>
2022-11-17 11:03:09 +0100

    UDP client server fonctionne en local

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,37 @@
 package org.example;
 
+import org.example.NetworkManager.ClientUDP;
+import org.example.NetworkManager.ServerUDP;
+
+import java.net.SocketException;
+
 public class Main {
+
     public static void main(String[] args) {
-        System.out.println("salut");
+        ClientUDP client;
+        ServerUDP serv;
+        String echo;
+
+        try {
+            serv = new ServerUDP();
+        } catch (SocketException e) {
+            throw new RuntimeException(e);
+        }
+
+        serv.start();
+
+        try {
+            client = new ClientUDP();
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+
+        echo = client.sendEcho("hello server");
+        System.out.println(echo);
+        echo = client.sendEcho("server is working");
+        System.out.println(echo);
+
+        client.close();
+
     }
 }
