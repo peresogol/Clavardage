3725255db50f3216089d6e297be20dd2d461df34
VassiliKan <sachacauli@gmail.com>
2023-01-27 02:08:31 +0100

    Launcher class created (real one, not for local test)

--- src/main/java/org/example/tests/Launcher.java
+++ src/main/java/org/example/tests/Launcher.java
@@ -1,39 +1,74 @@
 package org.example.tests;
 
-import org.example.GUI.EnterUsername;
+import org.example.utils.FormatServiceMessage;
 import org.example.Managers.ThreadManager;
 import org.example.NetworkManager.ClientUDP;
 import org.example.NetworkManager.NetworkManager;
 import org.example.NetworkManager.ServerUDP;
-import org.example.utils.FormatServiceMessage;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
 
 public class Launcher {
 
     private static ServerUDP serv;
-    public static NetworkManager networkManager;
+    private static NetworkManager networkManager;
     private static ThreadManager threadManager;
+    private static JPanel panel;
+    private static JFrame frame;
+    private static JPanel inputPanel;
+    private static JTextField textField;
+
 
     public static void main(String[] args) {
-        new EnterUsername("Veuillez saisir un nom d'utilisateur :");
-    }
+        frame = new JFrame("Choix username");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(300, 200);
 
-    public void startApp(String text) {
-        threadManager = new ThreadManager(5500);
-        threadManager.start();
-        networkManager = new NetworkManager(text, 6000);
-        startUDPServer();
+        panel = new JPanel();
+        panel.setLayout(new BorderLayout());
+
+        JLabel label = new JLabel("Veuillez saisir un nom d'utilisateur :");
+        panel.add(label, BorderLayout.NORTH);
+
+        inputPanel = new JPanel();
+        inputPanel.setLayout(new FlowLayout());
+        textField = new JTextField(20);
+        inputPanel.add(textField);
+
+        JButton button = new JButton("Valider");
+        inputPanel.add(button);
+        button.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                String tmp = textField.getText();
+                if (!tmp.equals("")) {
+                    threadManager = new ThreadManager(5500);
+                    threadManager.start();
+                    networkManager = new NetworkManager(tmp, 5000);
+                    startUDPServer();
+                }
+            }
+        });
+
+        panel.add(inputPanel, BorderLayout.CENTER);
+        frame.add(panel);
+        frame.setVisible(true);
     }
 
     // Start Server UDP
-    public static void startUDPServer(){
+    public static void startUDPServer() {
         serv = new ServerUDP(5000, packet -> networkManager.msgUDPhandler(packet));
         serv.start();
         discoverNetwork();
     }
 
     // Send broadcast to discover network
-    public static void discoverNetwork(){
+    public static void discoverNetwork() {
         String msg = FormatServiceMessage.msgGetConnectedUsers();
         ClientUDP.sendBroadcast(msg);
     }
 }
+
